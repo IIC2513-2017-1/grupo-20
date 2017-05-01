6615b67bbce22484872cb05f05a24a8159d2e654
ctalvarez <ctalvarez@uc.cl>
2017-04-30 21:14:16 -0300

    home view

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   root 'welcome#homepage'
   #get 'welcome/homepage'
 
-  resources :users, param: :username do
-    resources :rifas
-  end
+  # resources :users, param: :username do
+  #   resources :rifas
+  # end
+  resources :users
+  resources :rifas
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20170430215325_add_user_to_rifas.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToRifas < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :rifas, :user, foreign_key: true
-  end
-end
