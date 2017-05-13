75f421b25e0dc738b079918f077156ea52d57752
ctalvarez <ctalvarez@uc.cl>
2017-05-13 18:26:28 -0300

    has_secure_pass

---
+++ db/migrate/20170513212009_add_password_and_passwor_digest_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordAndPassworDigestToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :password, :string
+    add_column :users, :password_digest, :string
+  end
+end
