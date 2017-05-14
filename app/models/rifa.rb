97ffe1d5facfcbf1baa358fd6df288e5914ac6b6
ctalvarez <ctalvarez@uc.cl>
2017-05-14 17:05:19 -0400

    views y models

--- app/models/rifa.rb
+++ app/models/rifa.rb
@@ -1,5 +1,8 @@
 class Rifa < ApplicationRecord
   belongs_to :user
   has_many :prizes
-  
+
+  validates :end_date > (DateTime.now + 1.week)  # DateTime.now == crated_at
+  validates :end_date < (DateTime.now + 6.months)
+
 end
