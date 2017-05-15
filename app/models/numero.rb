452ea1b9be942d1ceab0e727d4082aad59cee155
Niotac <mieynaudi@uc.cl>
2017-05-14 23:51:19 -0400

    validaciones rifas

--- app/models/numero.rb
+++ app/models/numero.rb
@@ -1,4 +1,6 @@
 class Numero < ApplicationRecord
   belongs_to :rifa
   belongs_to :user
+
+  validates :number, presence: true, uniqueness: true
 end
--- app/models/rifa.rb
+++ app/models/rifa.rb
@@ -5,6 +5,7 @@ class Rifa < ApplicationRecord
 
   validates :end_date , date: { after: (DateTime.now + 1.week)}  # DateTime.now == crated_at
   validates :end_date , date: { before: (DateTime.now + 6.months)}  # DateTime.now == crated_at
-
+  validates :numbers, numericality: {only_integers: true, greater_than_or_equal_to: 10}
+  validates :price, numericality: {only_integers: true}
 
 end
