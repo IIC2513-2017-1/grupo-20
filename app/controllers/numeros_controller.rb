8313011acd4f68c2360e8e1b883103fb02d1a09f
Niotac <mieynaudi@uc.cl>
2017-05-24 15:52:53 -0400

    comprar rifas seleccion de numeros y se actualiza los numeros restantes de rifa

--- app/controllers/numeros_controller.rb
+++ app/controllers/numeros_controller.rb
@@ -2,10 +2,19 @@ class NumerosController < ApplicationController
   def new
     @numero = Numero.new
     @rifas = Rifa.find(params[:rifa_id])
+    @comprados = Array.new
+    compras = Numero.where({rifa_id: params[:rifa_id]})
+    for c in compras
+      @comprados << c.number
+    end
+
   end
   def create
     @numero = Numero.new(numero_params)
+    @rifas = Rifa.find(params[:rifa_id])
     if @numero.save
+      @rifas.numbers = @rifas.numbers - 1
+      @rifas.save
       redirect_to user_path(current_user)
     else
       redirect_to user_path(current_user), alert: 'No puedes comprar este numero.'
