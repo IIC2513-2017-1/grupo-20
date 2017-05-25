dfbd483d550ed9b4377a075d157ee32548f4ef99
ctalvarez <ctalvarez@uc.cl>
2017-05-25 01:20:23 -0400

    buscador

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-    @rifas = Rifa.all
+    @search = Rifa.search(params[:name_search], params[:premio_search],
+                          params[:min_price_search],
+                          params[:max_price_search],params[:date_search])
+
+  end
+
+  private
+  def searches_params
+    params.require(:rifa).permit(:name_search, :premio_search, :min_price_search, :max_price_search,
+                                 :date_search)
   end
 end
--- app/models/rifa.rb
+++ app/models/rifa.rb
@@ -8,4 +8,29 @@ class Rifa < ApplicationRecord
   validates :numbers, numericality: {only_integers: true, greater_than_or_equal_to: 10}
   validates :price, numericality: {only_integers: true}
 
+  def self.search(name, premio, min_price, max_price, date)
+    rifas = Rifa.all
+    if name.present?
+      rifas = rifas.where('title ilike ?', "%#{name}%" )
+    end
+    if premio.present?
+      rifas_prize = []
+      prizes = Prize.where('name ilike ?', "%#{premio}%" )
+      prizes.each do |p|
+        rifas_prize << p.rifa
+      end
+      rifas = rifas & rifas_prize
+
+    end
+    if min_price.present?
+      rifas = rifas.where('price > ?', min_price )
+    end
+    if max_price.present?
+      rifas = rifas.where('price < ?', max_price )
+    end
+    if date.present?
+      rifas = rifas.where('end_date < ?', "%#{DateTime.now + date.to_i.week}%" )
+    end
+    rifas
+  end
 end
